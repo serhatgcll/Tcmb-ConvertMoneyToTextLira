8e87b8f92afa1b923d685c46b454ec2bc4cfde00
serhatguclu <guclu2349@gmail.com>
2023-03-04 23:30:27 +0300

    tcmbden xml ile veri çekme

---
+++ Tcmb-ConvertMoneyToTextLira/Extension.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tcmb_ConvertMoneyToTextLira
+{
+   public static class Extension
+    {
+        public static string ConvertToMoneyTurkishLira(this string money)
+        {
+
+            decimal decPrice = Convert.ToDecimal(money);
+            string strPrice = decPrice.ToString("F2").Replace('.', ',');
+            string lira = strPrice.Substring(0, strPrice.IndexOf(','));
+            string kurus = strPrice.Substring(strPrice.IndexOf(',') + 1, 2);
+
+            string text = "";
+            string[] ones = { "", "BİR", "İKİ", "ÜÇ", "DÖRT", "BEŞ", "ALTI", "YEDİ", "SEKİZ", "DOKUZ" };
+            string[] tens = { "", "ON", "YİRMİ", "OTUZ", "KIRK", "ELLİ", "ALTMIŞ", "YETMİŞ", "SEKSEN", "DOKSAN" };
+            string[] thousands = { "KATRİLYON", "TRİLYON", "MİLYAR", "MİLYON", "BİN", "" };
+            int groupCount = 6;
+            lira = lira.PadLeft(groupCount * 3, '0');
+            string groupValue; 
+            for (int i = 0; i < groupCount * 3; i += 3)
+            {
+
+                groupValue = "";
+                if (lira.Substring(i, 1) != "0")
+
+                    groupValue += ones[Convert.ToInt32(lira.Substring(i, 1))] + "YÜZ";
+                if (groupValue == "BİRYÜZ")
+                    groupValue = "YÜZ";
+                groupValue += tens[Convert.ToInt32(lira.Substring(i + 1, 1))];
+                groupValue += ones[Convert.ToInt32(lira.Substring(i + 2, 1))];
+                if (groupValue != "")
+                    groupValue += "" + thousands[i / 3];
+                if (groupValue == "BİRBİN")
+                    groupValue = "BİN";
+                text += groupValue;
+
+
+
+            }
+            if (text != "")
+                text += "LİRA";
+            int yaziUzunlugu = text.Length;
+            if (kurus.Substring(0, 1) != "0")
+                text += tens[Convert.ToInt32(kurus.Substring(0, 1))];
+            if (kurus.Substring(1, 1) != "0")
+                text += ones[Convert.ToInt32(kurus.Substring(1, 1))];
+            if (text.Length > yaziUzunlugu)
+                text += "KURUŞ";
+            else text += "";
+
+            var newText = string.Format("#" + text + "#");
+            return newText;
+
+
+        }
+    }
+}
---
+++ Tcmb-ConvertMoneyToTextLira/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Xml;
+using System.Globalization;
+
+namespace Tcmb_ConvertMoneyToTextLira
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string today = "http://www.tcmb.gov.tr/kurlar/today.xml";
+
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(today);
+            DateTime @date = Convert.ToDateTime(xmlDoc.SelectSingleNode("//Tarih_Date").Attributes["Tarih"].Value);
+
+            string usd = xmlDoc.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteSelling").InnerXml;
+            string eur = xmlDoc.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteSelling").InnerXml;
+
+
+            double sonucUsd = double.Parse(usd,CultureInfo.InvariantCulture);
+            double sonucEuro = double.Parse(eur, CultureInfo.InvariantCulture);
+                                                                                      
+
+
+            Console.WriteLine("Seçiminizi Yapınız.1.Dolar,2.Euro");
+            double secilecekKur = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Tlye çevirelecek döviz tutarını giriniz. ");
+            double money = Convert.ToDouble(Console.ReadLine());
+
+
+
+            if (secilecekKur == 1)
+            {
+                double result = sonucUsd * money;
+                Console.WriteLine($"{@date.ToShortDateString()} tarihinde Tl türünden fiyatı :" + result + " \n" + "Yazıyla sadece :" + Convert.ToString(result).ConvertToMoneyTurkishLira());
+            }
+            if (secilecekKur == 2)
+            {
+                double result = sonucEuro * money;
+                Console.WriteLine($"{@date.ToShortDateString()} tarihinde Tl türünden fiyatı :" + result + " \n" + "Yazıyla sadece :" + Convert.ToString(result).ConvertToMoneyTurkishLira());
+            }
+
+        }
+    }
+  }
